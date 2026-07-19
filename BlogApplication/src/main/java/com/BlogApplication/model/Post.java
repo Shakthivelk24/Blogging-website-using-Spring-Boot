4647bdfacca4917b5b4516f67d19aa88e81156e8
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-07-19 20:43:07 +0530

    Created Post model in MySQL

---
+++ BlogApplication/src/main/java/com/BlogApplication/model/Post.java
@@ -0,0 +1,16 @@
+package com.BlogApplication.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Post{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(length = 1000)
+    private String content;
+    private String author;
+}
