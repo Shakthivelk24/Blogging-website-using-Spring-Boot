4f7a0b03639d10bd419f6cb587745fbd0ade9f31
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-07-21 20:47:06 +0530

    Updated the main.jsx with the context and Brower router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
+import UserContext from "./context/UserContext.jsx";
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <UserContext>
+        <App />
+      </UserContext>
+    </BrowserRouter>
   </StrictMode>,
 )
+
