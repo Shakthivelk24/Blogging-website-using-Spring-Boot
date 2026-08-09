58c3eb5848662c6445c58fbe2b688c2cb44ab312
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-08-09 18:13:21 +0530

    Remove CrossOrigin annotation from PostController

--- BlogApplication/src/main/java/com/BlogApplication/Controller/PostController.java
+++ BlogApplication/src/main/java/com/BlogApplication/Controller/PostController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/posts")
-@CrossOrigin(origins = "http://localhost:5173") 
+
 public class PostController {
     
     @Autowired
