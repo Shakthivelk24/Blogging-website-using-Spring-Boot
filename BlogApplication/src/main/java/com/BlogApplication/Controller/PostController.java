74583584cda21205ff54460bdbd77f0c19f5501e
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-08-10 20:48:35 +0530

    Refactor PostController to include authentication in create and user posts methods

--- BlogApplication/src/main/java/com/BlogApplication/Controller/PostController.java
+++ BlogApplication/src/main/java/com/BlogApplication/Controller/PostController.java
@@ -2,42 +2,77 @@ package com.BlogApplication.Controller;
 
 import com.BlogApplication.model.Post;
 import com.BlogApplication.Service.PostService;
+
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-
-
 
 @RestController
 @RequestMapping("/api/posts")
-
 public class PostController {
-    
+
     @Autowired
     private PostService postService;
-    
+
+    // Get all posts
     @GetMapping
     public List<Post> getAllPosts() {
         return postService.getAllPosts();
     }
 
+    // Search posts
+    // GET /api/posts/search?keyword=java
+    @GetMapping("/search")
+    public List<Post> getSearchedPosts(
+            @RequestParam String keyword) {
+
+        return postService.getSearchedPosts(keyword);
+    }
+
+    // Get posts of logged-in user
+    // GET /api/posts/user
+    @GetMapping("/user")
+    public List<Post> getUserPosts(
+            Authentication authentication) {
+
+        String username = authentication.getName();
+
+        return postService.getUserPosts(username);
+    }
+
+    // Get post by ID
     @GetMapping("/{id}")
     public Post getPostById(@PathVariable Long id) {
         return postService.getPostById(id);
     }
 
+    // Create post
     @PostMapping
-    public Post createPost(@RequestBody Post post) {
+    public Post createPost(
+            @RequestBody Post post,
+            Authentication authentication) {
+
+        // Get author from JWT instead of trusting frontend
+        post.setAuthor(authentication.getName());
+
         return postService.createPost(post);
     }
 
+    // Update post
     @PutMapping("/{id}")
-    public Post updatePost(@PathVariable Long id, @RequestBody Post post) {
+    public Post updatePost(
+            @PathVariable Long id,
+            @RequestBody Post post) {
+
         return postService.updatePost(id, post);
     }
 
+    // Delete post
     @DeleteMapping("/{id}")
     public void deletePost(@PathVariable Long id) {
         postService.deletePost(id);
     }
 }
+
