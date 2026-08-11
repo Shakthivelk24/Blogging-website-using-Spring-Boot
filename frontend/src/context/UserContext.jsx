7f05997ea20b3e8e1a31cadf8e28f38a6d51b223
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-08-11 20:27:54 +0530

    Refactor UserContext to manage user data and loading state with API integration

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,17 +1,41 @@
 import { useState } from "react";
 import { userDataContext } from "./DataContext";
+import { useEffect } from "react";
+import axios from "axios";
 
 
 
 function UserContext({ children }) {
-  const serverUrl = "http://localhost:8083/api/posts";
+  const serverUrl = "http://localhost:8083";
   const [post, setPost] = useState(null);
+  const [userData, setUserData] = useState(null);
+  const [loadingUser, setLoadingUser] = useState(true);
+  const handleCurrentUser = async () => {
+    try {
+      const result = await axios.get(`${serverUrl}/api/user/current`, {
+        withCredentials: true,
+      });
+      setUserData(result.data);
+      setLoadingUser(false);
+      console.log("Current User Data:", result.data);
+    } catch (error) {
+      console.error("Error fetching current user data:", error);
+      setLoadingUser(false);
+    }
+  };
+  useEffect(() => {
+    handleCurrentUser();
+  }, []);
+  
   const value = {
     serverUrl,
     post,
     setPost,
+    userData,
+    setUserData,
+    loadingUser,
   };
-
+  
   return (
     <div>
       <userDataContext.Provider value={value}>
