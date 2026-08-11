2afe2916ebdea7453759007ab938edf9a8794278
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-08-11 20:27:28 +0530

    Refactor App component to enhance routing with user authentication and add new pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,57 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 import CreatePost from "./pages/CreatePost";
+import MyPost from "./pages/MyPost";
 import EditPost from "./pages/EditPost";
 import { Toaster } from "react-hot-toast";
-
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import PostDetails from "./pages/PostDetails";
 
 function App() {
   return (
     <>
       <Toaster />
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<IndexPage />} />
-          <Route path="/create-post" element={<CreatePost />} />
-          <Route path="/edit-post/:id" element={<EditPost />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route
+            path="/create-post"
+            element={
+              <ProtectedRoute>
+                <CreatePost />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/mypost"
+            element={
+              <ProtectedRoute>
+                <MyPost />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/edit-post/:id"
+            element={
+              <ProtectedRoute>
+                <EditPost />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/post/:id"
+            element={
+              <PostDetails />
+            }
+          />
         </Route>
       </Routes>
     </>
